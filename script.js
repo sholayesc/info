94bca7d3316026365482c9a96a23bbf8d490e242
sholayesc <65164515+sholayesc@users.noreply.github.com>
2023-10-12 09:34:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Filter projects by category
+const filterProjects = (category) => {
+    const projects = document.querySelectorAll('.project');
+    
+    projects.forEach((project) => {
+        if (category === 'all' || project.getAttribute('data-category') === category) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+    });
+};
+
+// Add event listeners to navigation links
+document.getElementById('all').addEventListener('click', () => filterProjects('all'));
+document.getElementById('web').addEventListener('click', () => filterProjects('web'));
+document.getElementById('design').addEventListener('click', () => filterProjects('design'));
+document.getElementById('other').addEventListener('click', () => filterProjects('other'));
 
